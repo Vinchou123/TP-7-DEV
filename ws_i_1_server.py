97962bad4340bf421faaa4a77fabce84fb4723f6
Vincenzo <vincent.luu@ynov.com>
2024-11-24 17:57:50 +0100

    TP7

--- ws_i_1_server.py
+++ ws_i_1_server.py
@@ -14,7 +14,7 @@ async def echo(websocket, path):
         print(f"An error occurred: {e}")
 
 async def main():
-    async with websockets.serve(echo, "10.2.2.2", 8888) as server:
+    async with websockets.serve(echo, "10.2.2.223", 8888) as server:
         await server.wait_closed()
 
 if __name__ == "__main__":
