76c8e86382c1cee63716f89b8ac7fc57825e70c1
Vincenzo <vincent.luu@ynov.com>
2024-11-24 17:47:12 +0100

    TP7

--- ws_i_1_server.py
+++ ws_i_1_server.py
@@ -2,11 +2,16 @@ import asyncio
 import websockets
 
 async def echo(websocket, path):
-    async for message in websocket:
-        print(f"Received message: {message}")
-        response = f"Hello client! Received \"{message}\""
-        await websocket.send(response)
-        print(f"Sent response: {response}")
+    try:
+        async for message in websocket:
+            print(f"Received message: {message}")
+            response = f"Hello client! Received \"{message}\""
+            await websocket.send(response)
+            print(f"Sent response: {response}")
+    except websockets.exceptions.ConnectionClosedError as e:
+        print(f"Connection closed error: {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 async def main():
     async with websockets.serve(echo, "10.2.2.2", 8888) as server:
