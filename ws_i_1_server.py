d0158ac5d2472aa4852aa5431ceda14195703b5a
Vincenzo <vincent.luu@ynov.com>
2024-11-24 18:28:51 +0100

    TP7

--- ws_i_1_server.py
+++ ws_i_1_server.py
@@ -16,10 +16,13 @@ async def handle_client(websocket, path):
         print(f"Erreur dans handle_client : {e}")
 
 async def main():
-
-    async with websockets.serve(handle_client, "10.2.2.2", 8888):
-        print("Serveur WebSocket démarré sur ws://10.2.2.2:8888")
-        await asyncio.Future()
+    server = await websockets.serve(
+        handle_client,
+        "10.2.2.2",
+        8888             
+    )
+    print("Serveur WebSocket démarré sur ws://10.2.2.2:8888")
+    await asyncio.Future()
 
 if __name__ == "__main__":
     asyncio.run(main())
