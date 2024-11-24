e6c026c3ec49fd07a7718393cca655389833a2c6
Vincenzo <vincent.luu@ynov.com>
2024-11-24 18:25:59 +0100

    TP7

--- ws_i_1_client.py
+++ ws_i_1_client.py
@@ -4,6 +4,7 @@ import websockets
 async def send_message():
     try:
         async with websockets.connect("ws://10.2.2.2:8888") as websocket:
+            print("Connexion établie avec le serveur.")
             while True:
                 message = input("Entrez un message à envoyer au serveur (ou 'quit' pour terminer) : ")
                 
@@ -12,7 +13,8 @@ async def send_message():
                     break
                 
                 await websocket.send(message)
-                
+                print(f"Message envoyé : {message}")
+
                 response = await websocket.recv()
                 print(f"Réponse du serveur : {response}")
     except Exception as e:
--- ws_i_1_server.py
+++ ws_i_1_server.py
@@ -2,20 +2,21 @@ import asyncio
 import websockets
 
 async def handle_client(websocket, path):
+    print(f"Connexion établie avec : {websocket.remote_address}")
     try:
         while True:
             message = await websocket.recv()
-            print(f"Message reçu du client : {message}")
-            
+            print(f"Message reçu : {message}")
+
             response = f"Hello client! Received \"{message}\""
-            
             await websocket.send(response)
     except websockets.ConnectionClosedOK:
-        print("Connexion fermée par le client.")
+        print(f"Connexion fermée par le client : {websocket.remote_address}")
     except Exception as e:
-        print(f"Erreur dans le gestionnaire de connexion : {e}")
+        print(f"Erreur dans handle_client : {e}")
 
 async def main():
+
     async with websockets.serve(handle_client, "10.2.2.2", 8888):
         print("Serveur WebSocket démarré sur ws://10.2.2.2:8888")
         await asyncio.Future()
