b183990b317ffea03bffe23506038d172aab59a4
Vincenzo <vincent.luu@ynov.com>
2024-11-24 18:37:02 +0100

    TP7

--- ws_i_1_server.py
+++ ws_i_1_server.py
@@ -1,16 +1,16 @@
 import asyncio
 import websockets
 
-async def hello(websocket, path):
+async def hello(websocket):
     name = await websocket.recv()
     print(f"<<< {name}")
-    greeting = f"Hello {name}!"
+    greeting = f"Hello client ! Received \"{name}\""
     await websocket.send(greeting)
     print(f">>> {greeting}")
 
 
 async def main():
-    async with websockets.serve(hello, "10.2.2.2", 8888): 
+    async with websockets.serve(hello, "10.2.2.2", 8888):
         await asyncio.Future()
 
 if __name__ == "__main__":
